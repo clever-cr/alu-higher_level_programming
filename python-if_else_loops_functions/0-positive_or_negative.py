87492a0a354d7e076f165e049c4e742dc6a7218e
clever-cr <cleverumuhuza@gmail.com>
2022-10-24 23:52:14 -0700

    check if a number is negative or positive

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,7 +3,8 @@ import random
 number = ranom.randit(-10,10)
 if number > 0:
     print(f'{number}is positive')
-else if number == 0:
+elif number == 0:
     print(f'{number}is zero')
 else:
     print(f'{number}is negative')
+
