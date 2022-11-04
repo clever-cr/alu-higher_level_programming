5c0d5f1b03a461ec53e682f4b3bd53586bce233f
clever-cr <cleverumuhuza@gmail.com>
2022-11-04 19:23:20 +0200

    new list

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/python3
+def search_replace(my_list, search, replace):
+    list = my_list.copy()
+    if not my_list:
+        return list
+    for item in range(len(my_list)):
+        if list[item] == search:
+            list[item] = replace
+    return list
