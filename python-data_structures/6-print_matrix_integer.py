5770107c02625a8dc36ec310ffe5d355b6266668
clever-cr <cleverumuhuza@gmail.com>
2022-11-04 09:27:26 +0200

    mstrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for value in matrix
+        if not value:
+            print()
+        else:
+            for i in row:
+                if num == row[-1]:
+                    print('{:d}'.format(num))
+                else:
+                    print('{:d}'.format(num), end=" ")
