df5e41157c12d91ad6da1935615953c7c1b04748
clever-cr <cleverumuhuza@gmail.com>
2022-11-04 18:36:02 +0200

    multiples of 2

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def disible_by_2(my_list=[]):
+    d = []
+    for j in my_list:
+        if j % 2 == 0:
+            d.append(True)
+        else:
+            d.append(False) 
+    return d
