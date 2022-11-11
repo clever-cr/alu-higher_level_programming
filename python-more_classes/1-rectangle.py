5c260d1b83bf2a6ea5a141f237bbb24a1cf01f2b
clever-cr <cleverumuhuza@gmail.com>
2022-11-11 14:52:11 +0200

    fix syntax error

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
 
     """class Rectangle"""
-    def __init__(self, width=0, heaight=0):
+    def __init__(self, width=0, height=0):
         """initialize width and height"""
         self.width = width
         self.height = height
