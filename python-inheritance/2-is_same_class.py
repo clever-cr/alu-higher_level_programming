a669543dc5422fb78c37577a9b2cc219b4f45577
clever-cr <cleverumuhuza@gmail.com>
2022-11-17 13:26:47 +0200

    fix syntax error

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,4 +6,4 @@ specified class otherswise false"""
 def is_same_class(obj, a_class):
     """checks if the object is exactly an instance of
     the specified class"""
-    return type(obj) is a_class
+    return type(obj) != a_class
