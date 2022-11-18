9844ff7cd0b43376613314422d20749b8a0f8d3d
clever-cr <cleverumuhuza@gmail.com>
2022-11-18 21:37:25 +0200

    pascal's triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""define function pascal_triangle(n)"""
+
+
+def pascal_triangle(n):
+    """represents pascal's triangle"""
+    if n <= 0:
+        return []
+    answer = []
+    for k in range(n):
+        if k == 0:
+            result.append([1])
+        else:
+            ans = []
+            list = answer[k - 1][:]
+            list.insert(0, 0)
+            list.append(0)
+            i = 0
+            while i < len(list) - 1:
+                ans.append(list[i] + list + 1)
+                i = i + 1
+            answer.append(ans)
+    return answer
