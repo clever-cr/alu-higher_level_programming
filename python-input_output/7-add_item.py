c50881564a58b231b0888ed873aac5c6523e4140
clever-cr <cleverumuhuza@gmail.com>
2022-11-18 12:45:52 +0200

    fix error

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """a script load and save file"""
+
+
 import sys
-save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file.py
-load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file.py
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file.py
+load_from_json_file = __import__("6-load_from_json_file")\
+    .load_from_json_file.py
 
 file_name = "add_item.json"
 try:
