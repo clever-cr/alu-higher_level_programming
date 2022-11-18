9d03b147141f16f0eb66c295865f11fbc2be9f30
clever-cr <cleverumuhuza@gmail.com>
2022-11-18 11:14:30 +0200

    return json

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function to_json_string(my_obj)"""
+import json
+
+
+def to_json_string(my_obj):
+    """function that returns the json representation of an object"""
+    return json.dumps(my_obj)
