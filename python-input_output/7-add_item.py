6c558ef0eed1d4ce84378868aa9e8c6cfc366e54
clever-cr <cleverumuhuza@gmail.com>
2022-11-18 12:32:46 +0200

    load ,add ,save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/pyhton3
+"""a script that adds all arguments to a Python
+list and save them to a file"""
+import sys
+save_to_json_file = __import__("5-save_to_json_file.py").5-save_to_json_file.py
+load_from_json_file = __import__("6-load_from_json_file.py").6-load_from_json_file.py
+
+file_name = "add_item.json"
+try:
+    filelist = load_from_json_file(file_name)
+    save_to_json_file(filelist + sys.argv[1:], file_name)
+except FileNotFoundError:
+    save_to_json_file(sys.argv[1:], file_name)
