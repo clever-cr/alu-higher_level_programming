60ff25f29737a8fe88038693498c23d0f0242677
clever-cr <cleverumuhuza@gmail.com>
2022-11-18 10:58:02 +0200

    write file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pyhton3
+"""function write_file"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file and returns number of characters"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
