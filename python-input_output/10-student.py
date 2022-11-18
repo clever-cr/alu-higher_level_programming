8ea9e73767bd578ced51774a59ff6fa749605d2b
clever-cr <cleverumuhuza@gmail.com>
2022-11-18 21:14:56 +0200

    class student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""define class Student"""
+
+class Student:
+    """define student instance attributes"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attributes=None):
+        """retrieves dictionary representation"""
+        json = dict(vars(self))
+        if type(attributes) == list:
+            for l in list(json):
+                if l not in attributes:
+                    json.pop(l, None)
+        return json
+
+
+        
+        return vars(self)
