c09dc57b07048ccccd9e349b4246000b4b42fdb3
clever-cr <cleverumuhuza@gmail.com>
2022-11-23 20:21:39 +0200

    fix error

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,9 +4,9 @@
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as res:
+    with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as res:
         body_res = res.read()
         print("Body response:")
-        print("\t- type:{}".format(type(body_res)))
+        print("\t- type: {}".format(type(body_res)))
         print("\t- content: {}".format(body_res))
         print("\t- utf8 content: {}".format(body_res.decode("utf-8")))
