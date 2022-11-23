30f93f18eafa70d772ba3e6a7135c4b2406b421d
clever-cr <cleverumuhuza@gmail.com>
2022-11-23 09:10:14 +0200

    fix error

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """script that fetches https://alu-intranet.hbtn.io/status"""
 
+import urllib.request
+
 if __name__ = "__main__":
     with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as res:
         body_res = res.read()
