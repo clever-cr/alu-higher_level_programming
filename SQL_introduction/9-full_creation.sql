a1bedd37ead4bf4af3a78e045fe87342407879ff
clever-cr <cleverumuhuza@gmail.com>
2022-11-25 08:37:45 +0200

    full creation

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- creates second table in the database
+CREATE TABLE IF NOT EXISTS second_table(id INT,name VARCHAR(256),score INT);
+INSERT second_table VALUES (1, 'John', 10), (2, Alex, 3), (3, 'Bob', 14), (4, 'George', 8);
