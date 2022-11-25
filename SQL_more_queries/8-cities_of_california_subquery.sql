c0e308c99edf2e2b947cac4fbd36ac6c99c71f6a
clever-cr <cleverumuhuza@gmail.com>
2022-11-25 19:12:51 +0200

    fix error

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- lists all the cities of Callifornia
-SELECT id,name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'Calofornia');
+SELECT id,name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California');
