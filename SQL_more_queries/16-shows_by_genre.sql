b8e46508dc85792132b297383140bc4649a36bbf
clever-cr <cleverumuhuza@gmail.com>
2022-11-25 19:03:07 +0200

    fix error

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -3,6 +3,6 @@ SELECT tv_shows.title, tv_genres.name
 FROM tv_genres
 JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-RIGHT JOIN tv_show_genres
+RIGHT JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC
