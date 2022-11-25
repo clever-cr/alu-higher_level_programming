c935a621351be072114ace4ccb0aaa8da3ec98ef
clever-cr <cleverumuhuza@gmail.com>
2022-11-25 16:44:52 +0200

    fix error

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- list all genres from hbtn_0d_tvshows and display shows linked
 SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
-JOIN tv_shows_genres
+JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
