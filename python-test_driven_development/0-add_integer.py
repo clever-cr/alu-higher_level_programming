547b4caa4af5539922310548b4d8ed0d513a9c77
clever-cr <cleverumuhuza@gmail.com>
2022-12-05 20:49:23 +0200

    add integer

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Function that adds 2 numbers"""
+
+
+def add_integer(a, b=98):
+    """add a and b"""
+    if type(a) != float and type(a) != int:
+        error = "a must be an integer"
+        raise TypeError(error)
+    elif type(b) != float and type(b) !=int:
+        error2 = "b must be an integer"
+        raise TypeError(error2)
+    return int(a) + int(b)
