c7e3eda12fac0fb19eaa136a98b0f44068933f03
clever-cr <cleverumuhuza@gmail.com>
2022-12-05 20:53:31 +0200

    fix PEP8 error

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,7 +7,7 @@ def add_integer(a, b=98):
     if type(a) != float and type(a) != int:
         error = "a must be an integer"
         raise TypeError(error)
-    elif type(b) != float and type(b) !=int:
+    elif type(b) != float and type(b) != int:
         error2 = "b must be an integer"
         raise TypeError(error2)
     return int(a) + int(b)
