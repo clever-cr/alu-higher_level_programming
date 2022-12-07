5a93b10801286da50afd88cfcdfea3b6b48b3b4e
clever-cr <cleverumuhuza@gmail.com>
2022-12-07 20:16:38 +0200

    test max

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pyhthon3
+"""unittest module"""
+
+import unittest
+
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMax(unittest.TestCase):
+    """test max"""
+
+    def test_max(self):
+        """test max function"""
+        self.assertEqual(max_integer([2,1,3]), 3)
+        self.assertEqual(max_integer([-3,-4,-1]), -1)
+        self.assertEqual(max_integer([]), None)
