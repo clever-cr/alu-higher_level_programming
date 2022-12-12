1c7d0e80239a7cd79542871beea9e2fcc402fc47
clever-cr <cleverumuhuza@gmail.com>
2022-12-12 21:18:06 +0200

    add setter and getter

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """class rectangle"""
-    
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """define class rectangle"""
         super().__init__(id)
@@ -13,3 +13,55 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise valueError("width must be > 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(Self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise valueError("height must be > 0")
+        else:
+            self.__height = value
+    
+    @property
+    def x(self)
+        return self.__x
+    
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise valueError("x must be >= 0")
+        else:
+            self.__x = value
+
+@property
+def y(self):
+    return self.__y
+
+@y.setter
+def y(self, value):
+    if type(value) != int:
+        raise TypeError("y must be an integer")
+    elif value < 0:
+        raise valueError("y must be >= 0")
+    else:
+        self.__y = value
