9bec4613c8827d04682325234fd4a6cb53fcea76
clever-cr <cleverumuhuza@gmail.com>
2022-12-12 20:07:22 +0200

    class Base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""define class base"""
+
+import json
+import csv
+import turtle
+
+
+class Base:
+    """class base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """instance attribute"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
