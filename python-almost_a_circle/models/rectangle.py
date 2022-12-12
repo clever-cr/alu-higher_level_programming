792f99ecc96f2fce1fe5a3a7108cbdd82577baa4
clever-cr <cleverumuhuza@gmail.com>
2022-12-12 20:24:15 +0200

    add rectangle class

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/pyhton3
+"""class rectangle"""
+from models.base import Base
+
+class Rectangle(Base):
+    """class rectangle"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """define class rectangle"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
