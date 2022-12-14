1c3eaf1372703974fdbc9c8dd147ca64227f7c90
clever-cr <cleverumuhuza@gmail.com>
2022-12-14 15:26:50 +0200

    fixing pep8 error

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,12 +7,15 @@ if __name__ == '__main__':
     user = sys.argv[1]
     password = sys.argv[2]
     name = sys.argv[3]
-    my_db = MySQLdb.connect(host='localhost',
-                            port=3306,
-                            user=user,
-                            code=password,
-                            database=my_db)
-    cursor = my_db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY "N%" ORDER BY id ASC")
-    For state in cursor.fetchall():
-        print(i)
+    database = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=user,
+        password=password,
+        database=name
+    )
+    cursor = database.cursor()
+    cursor.execute('SELECT * FROM states WHERE name LIKE BINARY "N%" ORDER BY id ASC')
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
