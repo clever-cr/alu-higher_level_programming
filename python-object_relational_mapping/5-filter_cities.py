132c4cdaa85e1abe4b53d28848e2e5d292eb1ecc
clever-cr <cleverumuhuza@gmail.com>
2022-12-14 19:35:53 +0200

    filter cities by state

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/pyhton3
+"""list all cities of the state"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    password = sys.argv[2]
+    name = sys.argv[3]
+    state = sys.argv[4]
+    database = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=user,
+        password=password,
+        database=name
+    )
+    cursor = database.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
+        JOIN staes ON cities.states_id=states.id WHERE states.name = %s \
+            ORDER BY cities.id ASC", (state,))
+    cities = cursor.fetchall()
+    print(", ".join([city[0] for city in cities]))
