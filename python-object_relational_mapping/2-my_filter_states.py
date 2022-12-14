5502c68743ecd2e9ed5214c3d072b6bfb92045a0
clever-cr <cleverumuhuza@gmail.com>
2022-12-14 15:43:11 +0200

    filter states

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""script that displays all values in the states table """
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    password = sys.argv[2]
+    name = sys.argv[3]
+    state = sys.argv[4]
+    database = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=user,
+        password=password,
+        database=name
+    )
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM states WHERE BINARY name = {}".format(state))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
