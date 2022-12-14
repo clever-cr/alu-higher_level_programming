aab8df0157eee945bca44b08039fbd2eeef2c4ea
clever-cr <cleverumuhuza@gmail.com>
2022-12-14 15:11:39 +0200

    fix errors

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,11 +7,11 @@ if __name__ == '__main__':
     user = sys.argv[1]
     password = sys.argv[2]
     name = sys.argv[3]
-    my_db = MySQLdb.connect(host='localhost',
+    database = MySQLdb.connect(host='localhost',
                             port=3306,
                             user=user,
                             code=password,
-                            my_db=database)
+                            database=name)
     cursor = my_db.cursor()
     cursor.execute("SELECT * FROM states ")
     states = cursor.fetchall()
