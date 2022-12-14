2d5ada519d8a3471bb06f7d36732e29e94e3956a
clever-cr <cleverumuhuza@gmail.com>
2022-12-14 20:26:29 +0200

    fix error

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class State and instance base"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declalarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """instance Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
