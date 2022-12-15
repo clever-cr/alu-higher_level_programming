55b4e1bcacb7b213afcb0fef2e6775f023c892cc
clever-cr <cleverumuhuza@gmail.com>
2022-12-15 15:44:11 +0200

    fix error

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,11 +10,7 @@ if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
     name = sys.argv[3]
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.formart(
-                        user,
-                        password,
-                        name),
-                        pool_pre_ping=True)
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.formart(user,password,name),pool_pre_ping=True)
     Base.metadata.create_all(eng)
     ssn = sessionmaker(bind=eng)
     session = ssn()
