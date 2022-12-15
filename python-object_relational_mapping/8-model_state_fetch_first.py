6af582b9cfe6486b1899f9c3fd1e79075b0ad5cd
clever-cr <cleverumuhuza@gmail.com>
2022-12-15 16:10:05 +0200

    first state

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,4 +10,15 @@ if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
     name = sys.argv[3]
-    eng = create_engine
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                        user,
+                        password,
+                        name),
+                        pool_pre_ping=True)
+    Base.metadata.create_all(eng)
+    ssn = sessionmaker(bind=eng)
+    session = ssn()
+    states = session.query(State)\
+        .filter(State.name.like('%a%')).order_by(State.id).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
