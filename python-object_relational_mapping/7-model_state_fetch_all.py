a97ad37bfbdad9c4c303898062dd4914cdbb1f95
clever-cr <cleverumuhuza@gmail.com>
2022-12-15 15:54:24 +0200

    fix

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,10 +10,13 @@ if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
     name = sys.argv[3]
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.formart(user,password,name),pool_pre_ping=True)
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                        user,
+                        password,
+                        name),
+                        pool_pre_ping=True)
     Base.metadata.create_all(eng)
     ssn = sessionmaker(bind=eng)
     session = ssn()
-    print(session.query(State).order_by(State.id).all())
     for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
