17029989daf672f3966acea4ac8d3e18e66eab95
clever-cr <cleverumuhuza@gmail.com>
2022-12-16 12:54:18 +0200

    fix error

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -22,4 +22,4 @@ if __name__ == "__main__":
     cities = session.query(City, State).filter(City.state_id == State.id)\
         .order_by(City.id).all()
     for city, state in cities:
-        print(f"{state.name}: {city.id}: {city.name}")
+        print(f"{state.name}: ({city.id}) {city.name}")
