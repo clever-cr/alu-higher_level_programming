38e9d25732e3ac3f333cac8f41baa29ce5a171db
clever-cr <cleverumuhuza@gmail.com>
2022-12-16 11:33:54 +0200

    fix error

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -9,11 +9,11 @@ import sys
 if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
-    name = sys.argv[3]
+    db = sys.argv[3]
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
                         user,
                         password,
-                        name),
+                        db),
                         pool_pre_ping=True)
     Base.metadata.create_all(eng)
     ssn = sessionmaker(bind=eng)
