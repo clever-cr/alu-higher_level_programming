7014acfc5bc14d35bc89f1219bb82b1288733bdb
clever-cr <cleverumuhuza@gmail.com>
2022-12-16 13:55:14 +0200

    fix error

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -25,7 +25,7 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError("width must be an integer")
         elif value <= 0:
-            raise valueError("width must be > 0")
+            raise ValueError("width must be > 0")
         else:
             self.__width = value
 
@@ -40,7 +40,7 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError("height must be an integer")
         elif value <= 0:
-            raise valueError("height must be > 0")
+            raise ValueError("height must be > 0")
         else:
             self.__height = value
 
@@ -55,7 +55,7 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError("x must be an integer")
         elif value < 0:
-            raise valueError("x must be >= 0")
+            raise  ValueError("x must be >= 0")
         else:
             self.__x = value
 
@@ -70,6 +70,6 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError("y must be an integer")
         elif value < 0:
-            raise valueError("y must be >= 0")
+            raise ValueError("y must be >= 0")
         else:
             self.__y = value
