b088f0e5ea8df359292fe07c7342a674cfecf9e4
clever-cr <cleverumuhuza@gmail.com>
2022-12-16 20:33:41 +0200

    add json list dictionary representation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,3 +17,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Json representation of list dictionaries"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
