9d83c4af117b60229586845eba158fc4a5ed71d9
clever-cr <cleverumuhuza@gmail.com>
2022-12-16 14:50:43 +0200

    fix error

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -87,5 +87,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
-        return(f"[Rectangle] ({self.id}) {self.x}/{self.y}\
-             - {self.width}/{self.height}")
+        return(f"[Rectangle] ({self.id}) {self.x}/{self.y} \
+        - {self.width}/{self.height}")
