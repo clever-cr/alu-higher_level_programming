32f417ebb95e34965bdf810d786d89a48b0c6244
clever-cr <cleverumuhuza@gmail.com>
2022-12-16 19:50:18 +0200

    assign values

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,3 +10,14 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """define class"""
         super().__init__(size,size,x,y,id)
+
+    @property
+    def size(self):
+        """return size"""
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        """set size"""
+        self.width = value
+        self.height = value
