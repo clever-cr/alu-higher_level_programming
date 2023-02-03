da71504c3e66cc12e3fef5d4f55173188a96b5dc
clever-cr <cleverumuhuza@gmail.com>
2023-02-03 19:33:17 +0200

    fix error

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-function ar() {
-    if (arguments.length == 1) {
-        console.log("Argument found")
+
+let arguments = process.argv.slice(2);
+if (arguments.length == 1) {
+        console.log('Argument found')
     }
     else if (arguments.length == 0) {
-        console.log("No argument")
+        console.log('No argument')
     }
     else {
-        console.log("Arguments found")
+        console.log('Arguments found')
     }
-}
