8a2d237bfca2912072a5a129a816f5346db1d1c7
clever-cr <cleverumuhuza@gmail.com>
2023-02-09 15:46:29 +0200

    multi c

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const argument = process.argv[2];
+if (argument === undefined) {
+    console.log('Missing number of occurrences')
+}
+for (let i = 0; i < argument; i++) {
+    console.log('C is fun')
+}
