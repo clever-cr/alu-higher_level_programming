62b7adc845054082881496a54cf93b20641ba4dc
clever-cr <cleverumuhuza@gmail.com>
2023-02-09 19:40:19 +0200

    fix error

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,27 +1,27 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
+    constructor(w, h) {
+        if (w > 0 && h > 0) {
+            this.width = w;
+            this.height = h;
+        }
     }
-  }
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+    print() {
+        for (let i = 0; i < this.height; i++) {
+            console.log('X'.repeat(this.width));
+        }
     }
-  }
 
-  rotate () {
-    const swap = this.height;
-    this.width = this.height;
-    this.width = swap;
-  }
+    rotate() {
+        const swap = this.height;
+        this.height = this.width;
+        this.width = swap;
+    }
 
-  double () {
-    this.height *= 2;
-    this.width *= 2;
-  }
+    double() {
+        this.height *= 2;
+        this.width *= 2;
+    }
 }
 module.exports = Rectangle;
