3c83aaaa7619f1a94031efa417e0c9df84643392
clever-cr <cleverumuhuza@gmail.com>
2023-02-10 13:11:12 +0200

    fix error

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,7 +4,5 @@ const request = require('fs');
 request.writeFile(process.argv[2], process.argv[3], (error, data) => {
   if (error) {
     console.log(error);
-  } else {
-    console.log(data);
   }
 });
