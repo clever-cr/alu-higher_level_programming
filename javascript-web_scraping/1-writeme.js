3c811d3ad1a0dc33fe7ab05ae8e26cce88eccd54
clever-cr <cleverumuhuza@gmail.com>
2023-02-10 13:09:21 +0200

    write

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('fs');
+
+request.writeFile(process.argv[2], process.argv[3], (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
