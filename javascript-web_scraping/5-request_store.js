d57800b08d975c339984122f640f62dae39856cf
clever-cr <cleverumuhuza@gmail.com>
2023-02-10 18:33:05 +0200

    fix error

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+request.get(url, (error, response, body) => {
+    if (error) {
+        console.log(error);
+    } else {
+        fs.writeFile(process.argv[3], body, (error, response) => {
+            if (error) {
+                console.log(error);
+            }
+        });
+    }
+});
